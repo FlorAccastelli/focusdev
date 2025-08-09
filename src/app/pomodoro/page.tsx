a22aa10cf8fda04c8a742900418e998c2fc512c9
Florencia Accastelli <f.accastelli@gmail.com>
2025-08-09 11:48:49 -0300

    feat: initial project setup with base structure and layout

--- src/app/pomodoro/page.tsx
+++ src/app/pomodoro/page.tsx
@@ -1,10 +1,10 @@
-const HomePage = () => {
+const PomodoroPage = () => {
     return (
         <main className="p-6">
-            <h1 className="text-2xl font-bold">Home Page</h1>
+            <h1 className="text-2xl font-bold">POmodoro Page</h1>
             <p className="mt-2 text-sm text-slate-600">Sección pública (stub).</p>
         </main>
     );
 }
 
-export default HomePage;
+export default PomodoroPage;
